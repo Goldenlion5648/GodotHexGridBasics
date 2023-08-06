92fa9dad71319dd2ed9a1c29a9a8258de2a79d58
Goldenlion5648 <coboudinot@gmail.com>
2023-08-06 10:46:08 -0400

    video complete

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hex Grid, Tilemap, and Tileset Basics
+This is the project that goes with this video:
+
+https://youtu.be/1qmXFIJU1QE
+
+The project shows how to click on cells in a tilemap, and cycle through a cell's alternatives
---
+++ tile_map_controller.gd
@@ -0,0 +1,18 @@
+extends TileMap
+
+const main_layer = 0
+const main_atlas_id = 0
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			var global_clicked = event.position
+			var pos_clicked = local_to_map(to_local(global_clicked))
+			print(pos_clicked)
+			var current_atlas_coords = get_cell_atlas_coords(main_layer, pos_clicked)
+			var current_tile_alt = get_cell_alternative_tile(main_layer, pos_clicked)
+			var number_of_alts_for_clicked = tile_set.get_source(main_atlas_id)\
+								.get_alternative_tiles_count(current_atlas_coords)
+			set_cell(main_layer, pos_clicked, main_atlas_id, current_atlas_coords, 
+							(current_tile_alt + 1) %  number_of_alts_for_clicked)
+get_cell_atlas_coords()
