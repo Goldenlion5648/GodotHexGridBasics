7cc199afef83ea996ae5bf4269d08e79b46a3550
Noah Jay <Jayjader@users.noreply.github.com>
2023-09-06 12:32:22 +0200

    prevent error when clicking on empty tiles

--- tile_map_controller.gd
+++ tile_map_controller.gd
@@ -12,7 +12,8 @@ func _input(event):
 			print(pos_clicked)
 			var current_atlas_coords = get_cell_atlas_coords(main_layer, pos_clicked)
 			var current_tile_alt = get_cell_alternative_tile(main_layer, pos_clicked)
-			var number_of_alts_for_clicked = tile_set.get_source(main_atlas_id)\
-								.get_alternative_tiles_count(current_atlas_coords)
-			set_cell(main_layer, pos_clicked, main_atlas_id, current_atlas_coords, 
-							(current_tile_alt + 1) %  number_of_alts_for_clicked)
+			if current_tile_alt > -1:
+				var number_of_alts_for_clicked = tile_set.get_source(main_atlas_id)\
+						.get_alternative_tiles_count(current_atlas_coords)
+				set_cell(main_layer, pos_clicked, main_atlas_id, current_atlas_coords, 
+						(current_tile_alt + 1) %  number_of_alts_for_clicked)
